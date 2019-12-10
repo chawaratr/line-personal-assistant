30f81640c0f3038db1a5907d0d9bcdf99ea2dcfa
chawaratr <eak.cpe18@gmail.com>
2019-12-10 18:38:23 +0700

    uopd

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.post("/webhook", (req, res) => res.sendStatus(200));
 app.get("/", (req, res) => res.sendStatus(200));
+console.log("listening on port", port);
 app.listen(port);

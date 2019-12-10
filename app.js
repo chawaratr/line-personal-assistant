e95c492a25cd8bc4367c1db795e4224d192e8cfb
chawaratr <eak.cpe18@gmail.com>
2019-12-10 18:56:05 +0700

    new dialogflow app

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
 const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const port = process.env.PORT || 4000;
-app.use(bodyParser.urlencoded({ extended: false }));
+
+// Import the appropriate class
+const { WebhookClient } = require("dialogflow-fulfillment");
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.post("/webhook", (req, res) => {
-  let reply_token = req.body.events[0].replyToken;
-  let msg = req.body.events[0].message.text;
-  reply(reply_token, msg);
-  res.sendStatus(200);
-});
-app.listen(port);
-function reply(reply_token, msg) {
-  let headers = {
-    "Content-Type": "application/json",
-    Authorization:
-      "Bearer ccRkRBjEnApsCTQNaqF9CbHmO53dLeiqxK1PgXZ+K9qpWpmUhVYCjLQz6kNHL3TT8MB6aMaF6n/LD3uda0Cc/NrzcECh9cAqD637/AqX9K9JKP8CYcHAHB1Kx9F14SspHaT1V4vKwIAUC/nmX0dEzAdB04t89/1O/w1cDnyilFU="
-  };
-  let body = JSON.stringify({
-    replyToken: reply_token,
-    messages: [
-      {
-        type: "text",
-        text: msg
-      }
-    ]
+
+app.get("/", (req, res) => {
+  res.send({
+    success: true
   });
-  request.post(
-    {
-      url: "https://api.line.me/v2/bot/message/reply",
-      headers: headers,
-      body: body
-    },
-    (err, res, body) => {
-      console.log("status = " + res.statusCode);
-    }
-  );
-}
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("POST: /");
+  console.log("Body: ", req.body);
+
+  //Create an instance
+  const agent = new WebhookClient({
+    request: req,
+    response: res
+  });
+
+  //Test get value of WebhookClient
+  console.log("agentVersion: " + agent.agentVersion);
+  console.log("intent: " + agent.intent);
+  console.log("locale: " + agent.locale);
+  console.log("query: ", agent.query);
+  console.log("session: ", agent.session);
+
+  //Function Location
+  function location(agent) {
+    agent.add("Welcome to Thailand.");
+  }
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set("Location", location); // "Location" is once Intent Name of Dialogflow Agent
+  agent.handleRequest(intentMap);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
